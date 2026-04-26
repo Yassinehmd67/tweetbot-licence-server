4ed72c654fc0a468faf14aa724d4cc4700b30905
Yassinehmd67 <@example.com>
2026-04-26 04:50:33 +0000

    fix homepage served by api route

---
+++ api/home.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "index.html");
+  const html = fs.readFileSync(filePath, "utf8");
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  return res.status(200).send(html);
+}
